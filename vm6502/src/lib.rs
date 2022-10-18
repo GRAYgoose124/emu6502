a893e083ae1ce1557abc74c983025b4b1aac39e8
Grayson Miller <grayson.miller124@gmail.com>
2022-10-17 21:16:20 -0600

    Removed extraneous files.

--- vm6502/src/lib.rs
+++ vm6502/src/lib.rs
@@ -1,15 +1,12 @@
 use rand::prelude::*;
 
-mod memory;
 mod registers;
-mod stack;
 mod vm;
 mod utils;
 
 pub mod prelude {
-    pub use crate::memory::Memory;
     pub use crate::registers::Registers;
-    
+
     pub use crate::vm::prelude::*;
     pub use crate::utils::prelude::*;
 }
