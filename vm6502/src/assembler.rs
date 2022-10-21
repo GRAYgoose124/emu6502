e14f6e7e30d5c6be507573825b6e6a4b134b7fcf
Grayson Miller <grayson.miller124@gmail.com>
2022-10-21 20:57:46 +0000

    Documentation, mostly in vm6502/src/lib.rs

--- vm6502/src/assembler.rs
+++ vm6502/src/assembler.rs
@@ -1,3 +1,6 @@
+//! High performance 6502 assembler.
+//!
+// TODO Interpretation
 //use crate::prelude::*;
 
 pub mod prelude {}
--- vm6502/src/lib.rs
+++ vm6502/src/lib.rs
@@ -4,26 +4,44 @@
 //!
 //! It is designed to be used as a library, with no frontend.
 //!
+//! # Virtual machine
 //! The virtual machine is implemented as a struct, `VirtualMachine`, which contains
 //! all the state of the machine. The state is stored in a flat array, which is
 //! indexed by the `Registers` struct.
 //!
-//! The `VirtualMachine` has a flap memory map which the stack and heap index into.
-//! See more:
+//! ## Specifications
+//! ### Registers
+//! THere are 8 status flags, stored in the [registers](crate::prelude::Registers).
 //!
+//! S/N:  The B flag is not actually set on the real 6502 status register, instead it's configured in memory when it's pushed and pulled. (Presumably during BRK)
+//!
+//! ##### Flags
+//! ### Memory
+//! The [VirtualMachine](crate::prelude::VirtualMachine) has a flat memory map which the stack and heap index into.
+//!
+//! ### Addressing modes
+//! ### Instruction set
+//! [Instructions](crate::prelude::Instructions)
+//! ## Macros
+//! Several macros are provided for more easily interacting with the machine and wielding opcodes.
+//! [See more.](crate::utils)
+//!
+//! # !! In construction !!
+//! Also provided is an [assembler](crate::assembler) and a [programmer](crate::program).
 //#![deny(missing_docs)]
 
-mod assembler;
-mod program;
-mod utils;
-mod vm;
+pub mod assembler;
+pub mod program;
+pub mod utils;
+pub mod vm;
 
 pub mod prelude {
-    // Virtual machine implementation.
+    /// Virtual machine implementation prelude
     pub use crate::vm::prelude::*;
 
     // Virtual machine utilities and macros.
     pub use crate::program::prelude::*;
+
     pub use crate::utils::prelude::*;
 
     pub use crate::assembler::prelude::*;
